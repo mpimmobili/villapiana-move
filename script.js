1104b778d8ab5629dc7d85649b0ad00a1f730a71
mpimmobili <mp.immobili.01@gmail.com>
2025-10-27 13:10:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Registra il Service Worker per la modalità offline
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log("✅ Service Worker registrato"))
-    .catch(err => console.log("❌ Errore Service Worker:", err));
-}
